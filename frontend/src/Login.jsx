c56b532e63802751022f36f7072048077d55aee9
Neel Patel <patelneel0445@gmail.com>
2025-06-27 21:02:10 +0530

    Refactor Login component for improved readability and consistency in JSX syntax

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,22 +1,34 @@
 import React from "react";
-import styles from "./Login.module.css";;
+import styles from "./Login.module.css";
 
 const Login = () => {
   return (
-    <div className={styles.login-container}>
-      <div className={styles.login-box}>
+    <div className={styles["login-container"]}>
+      <div className={styles["login-box"]}>
         <h2 className={styles.h2}>Login</h2>
         <form>
-          <div className={styles.input-group}>
+          <div className={styles["input-group"]}>
             <label htmlFor="email">Username</label>
-            <input type="email" id="email" placeholder="Enter your Username" required />
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your Username"
+              required
+            />
           </div>
-          <div className={styles.input-group}>
+          <div className={styles["input-group"]}>
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Enter your password" required />
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              required
+            />
           </div>
-          <button className={styles.button} type="submit">Login</button>
-          <p className={styles.signup-link}>
+          <button className={styles.button} type="submit">
+            Login
+          </button>
+          <p className={styles["signup-link"]}>
             Don't have an account? <a href="/sign-up">Sign up</a>
           </p>
         </form>
