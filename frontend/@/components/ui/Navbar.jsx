e793770a6aeefaa899d004121e98328f31e32cfb
Hirjani <heerjani05@gmail.com>
2025-06-27 18:05:58 +0530

    .

---
+++ frontend/@/components/ui/Navbar.jsx
@@ -0,0 +1,45 @@
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@radix-ui/react-popover";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            <span className="text-blue-500">INTERN</span>
+            <span className="text-gray-700">SHALA</span>
+          </h1>
+        </div>
+        <div>
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Internships</li>
+            <li>Browse</li>
+          </ul>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </PopoverTrigger>
+            <div>
+              <h1>Hello</h1>
+            </div>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
